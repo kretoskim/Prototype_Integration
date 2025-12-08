35ec6ab795ee74bd35e8fd979f26a5c4cf095449
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2025-12-08 21:57:14 +0300

    Menu UI -> partially

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private GameObject introMenu;
+    [SerializeField] private GameObject settingsMenu;
+    
+}
