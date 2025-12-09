02b03f9240859c2deffa84a7dfbe944c0d754bb3
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2025-12-09 09:29:55 +0300

    Menu screens

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,5 +8,39 @@ public class MenuUI : MonoBehaviour
     [SerializeField] private Button quitButton;
     [SerializeField] private GameObject introMenu;
     [SerializeField] private GameObject settingsMenu;
-    
+
+    private void Awake()
+    {
+        if(introMenu != null)
+        {
+            introMenu.SetActive(true);
+        }
+        if(settingsMenu != null)
+        {
+            settingsMenu.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        //subscribe + safety check
+        if(playButton != null)
+        {
+            playButton.onClick.AddListener(OpenSettings);
+        }
+    }
+    private void OnDisable()
+    {
+        //unsubscribe to prevent memory leaks
+        if(playButton != null)
+        {
+            playButton.onClick.RemoveListener(OpenSettings);
+        }
+    }
+
+    private void OpenSettings()
+    {
+        introMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
 }
